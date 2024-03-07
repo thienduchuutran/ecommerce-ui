2b97b7a278fc6c59c897f7e3cd4e4a5bdc07568c
Duc <thienductranhuu2784@gmail.com>
2024-03-07 18:25:29 -0500

    config prettier

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+import myAlert from "~/myAlert"
+
+function Button() {
+    return <button onClick={myAlert}>Click</button>
+}
+
+export default Button
---
+++ src/myAlert.js
@@ -0,0 +1,5 @@
+const myAlert = () => {
+    alert('ho')
+}
+
+export default myAlert
